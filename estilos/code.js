899d19e4a15e2af30de7fba0f256e3865fa4aa47
celina <celinadgcontacto@gmail.com>
2022-07-29 00:19:24 -0300

    actualizacion js

--- estilos/code.js
+++ estilos/code.js
@@ -46,15 +46,19 @@ let nombreD = 'Verduras';
 const nombres = [nombreA,
     nombreB, nombreC, nombreD
 ];
-const nombreVerduras = ['Acelga', 'Ajo', 'Cebolla', 'Coliflor', 'Puerro', 'Berenjena', 'Calabaza', 'Tomate', 'Zanahoria', 'Lechuga', , 'Remolacha', 'Huevo'];
+const nombreVerduras = ['Acelga', 'Ajo', 'Cebolla', 'Coliflor', 'Puerro', 'Berenjena', 'Calabaza', 'Tomate', 'Zanahoria', 'Lechuga', 'Remolacha', 'Huevo'];
 const nombreFrutas = ['Limón', 'Arándano', 'Frutilla', 'Mandarina', 'Naranja', 'Pomelo', 'Sandía', 'Melón', 'Coco', 'Kiwi', 'Banana', 'Ananá', 'Cereza', 'Manzana', 'Ciruela', 'Uva', 'Pera', ];
+const mascotasProductos = ['Comida para perros', 'Comida para gatos', 'Shampoo para animales', 'Correa', 'Bozal', 'Juguete', 'Mordedor', 'Pecera'];
+const limpiezaProductos = ['Papel higiénico', 'Pasta dental', 'Cepillo de dientes', 'Hilo dental', 'Desodorante', 'Jabon', 'Shampoo', 'Acondicionador', 'Lavandina'];
+const bebidasProductos = ['Cerveza 1L', 'Gaseosa cola 2L', 'Agua mineral 600ml', 'Agua saborizada 1.5L', 'Fernet 1L', 'Gaseosa lima 2L', 'Gaseosa naranja 2L', 'Jugo de frutas 1L'];
+const regaleriaProductos = ['Vaso térmico', 'Equipo de mate', 'Mate', 'Cuadro', 'Jarron', 'Maceta'];
 
 
-const nombreA = {
+/*const nombreA = {
     nombre: nombreA,
     categoria: 'Mascotas',
 
-}
+}*/
 
 const prototipoCarrito = {
     agregarProducto: function (producto) {
@@ -68,3 +72,27 @@ const prototipoCarrito = {
         return this.productos.reduce((total, p) => total + p.precio, 0);
     }
 }
+
+let arrayDummy = [0];
+
+function listaCantidades(arrayCategoria) {
+    arrayDummy = [0];
+    for (let index = 1; index < arrayCategoria.length; index++) {
+        arrayDummy.push(0);
+    }
+    return arrayDummy;
+}
+
+class categoria {
+    constructor(nombre, productos, cantidadCompra){
+        this.nombre = nombre;
+        this.productos = productos;
+        this.cantidadCompra = cantidadCompra;
+    }
+}
+
+let verduras = new categoria("Verduras", nombreVerduras, listaCantidades(nombreVerduras));
+let mascotas = new categoria("Mascotas", mascotasProductos, listaCantidades(mascotasProductos));
+let limpieza = new categoria("Limpieza", limpiezaProductos, listaCantidades(limpiezaProductos));
+let bebidas = new categoria("Bebidas", bebidasProductos, listaCantidades(bebidasProductos));
+let regaleria = new categoria("Regaleria",regaleriaProductos, listaCantidades(regaleriaProductos));
