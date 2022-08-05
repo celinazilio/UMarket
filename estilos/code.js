3691dde2de419ec86cd9751b7ac06eed2a69a6ea
celina <celinadgcontacto@gmail.com>
2022-08-04 23:27:30 -0300

    arreglado lista

--- estilos/code.js
+++ estilos/code.js
@@ -43,6 +43,58 @@ let limpieza = new categoria("Limpieza", limpiezaProductos, listaCantidades(limp
 let bebidas = new categoria("Bebidas", bebidasProductos, listaCantidades(bebidasProductos));
 let regaleria = new categoria("Regaleria", regaleriaProductos, listaCantidades(regaleriaProductos));
 
+// button
+var listaComprasAdd = document.getElementsByTagName("li");
+var i;
+for (i = 0; i < listaComprasAdd.length; i++) {
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "listaCerrar";
+    span.appendChild(txt);
+    listaComprasAdd[i].appendChild(span);
+}
 
+// click
+var listaCerrar = document.getElementsByClassName("listaCerrar");
+var i;
+for (i = 0; i < listaCerrar.length; i++) {
+    listaCerrar[i].onclick = function () {
+        var div = this.parentElement;
+        div.style.display = "none";
+    }
+}
 
-document.addEventListener("keyup",(event)=>{ if(event.keyCode===13){addToDo()}});
+// "checked" 
+var list = document.querySelector('ul');
+list.addEventListener('click', function (ev) {
+    if (ev.target.tagName === 'LI') {
+        ev.target.classList.toggle('checked');
+    }
+}, false);
+
+//  new list item 
+function nuevoElemento() {
+    var li = document.createElement("li");
+    var inputValue = document.getElementById("myInput").value;
+    var t = document.createTextNode(inputValue);
+    li.appendChild(t);
+    if (inputValue === '') {
+        alert("¡No agregaste nada!");
+    } else {
+        document.getElementById("myUL").appendChild(li);
+    }
+    document.getElementById("myInput").value = "";
+
+    var span = document.createElement("span");
+    var txt = document.createTextNode("\u00D7");
+    span.className = "listaCerrar";
+    span.appendChild(txt);
+    li.appendChild(span);
+
+    for (i = 0; i < listaCerrar.length; i++) {
+        listaCerrar[i].onclick = function () {
+            var div = this.parentElement;
+            div.style.display = "none";
+        }
+    }
+}
