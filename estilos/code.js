9427738030f08fa56e9da7196281601f9457f62c
celina <celinadgcontacto@gmail.com>
2022-08-18 23:29:35 -0300

    libreria y correccion

--- estilos/code.js
+++ estilos/code.js
@@ -10,7 +10,7 @@ window.onload = function () {
 };
 
 
-// selectores
+// lista 
 const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
@@ -35,16 +35,61 @@ addBtn.addEventListener("click", (e) => {
 
 function addDeleteBtn() {
     const deleteBtn = document.createElement("button");
-    deleteBtn.textContent = "Borrar";
+    deleteBtn.textContent = "x";
     deleteBtn.className = "btn-delete";
     deleteBtn.addEventListener("click", (e) => {
         const item = e.target.parentElement;
         ul.removeChild(item);
-
         const items = document.querySelectorAll("li");
-
         items.length == 0 ? empty.style.display = "block" : console.log("borrados");
     });
 
     return deleteBtn;
 }
+
+
+const tareas = document.getElementById('tareas');
+const tareasBebidas = document.getElementById('tareasBebidas');
+const tareasRegaleria = document.getElementById('tareasRegaleria');
+const tareasVerduras = document.getElementById('tareasVerduras');
+const tareasLimpieza = document.getElementById('tareasLimpieza');
+
+Sortable.create(tareas, {
+    group: {
+        name: "lista-tareas",
+    },
+
+    animation: 150,
+    chosenClasss: "activate"
+});
+
+Sortable.create(tareasBebidas, {
+    group: {
+        name: "lista-tareas",
+       
+    },
+    animation: 150,
+    chosenClasss: "activate"
+});
+Sortable.create(tareasRegaleria, {
+    group: {
+        name: "lista-tareas",
+       
+    },
+    animation: 150,
+    chosenClasss: "activate"
+});
+Sortable.create(tareasLimpieza, {
+    group: {
+        name: "lista-tareas"
+    },
+    animation: 150,
+    chosenClasss: "activate"
+});
+Sortable.create(tareasVerduras, {
+    group: {
+        name: "lista-tareas"
+    },
+    animation: 150,
+    chosenClasss: "activate"
+});
