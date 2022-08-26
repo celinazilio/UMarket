344afcf177a2beaf2a65951d24e9ce5c0c889cef
celina <65210102+c3sbasss@users.noreply.github.com>
2022-08-26 03:21:16 -0300

    Add files via upload

--- estilos/code.js
+++ estilos/code.js
@@ -10,7 +10,8 @@ window.onload = function () {
 };
 
 
-// lista 
+/*
+
 const input = document.querySelector("input");
 const addBtn = document.querySelector(".btn-add");
 const ul = document.querySelector("ul");
@@ -66,7 +67,7 @@ Sortable.create(tareas, {
 Sortable.create(tareasBebidas, {
     group: {
         name: "lista-tareas",
-       
+
     },
     animation: 150,
     chosenClasss: "activate"
@@ -74,7 +75,7 @@ Sortable.create(tareasBebidas, {
 Sortable.create(tareasRegaleria, {
     group: {
         name: "lista-tareas",
-       
+
     },
     animation: 150,
     chosenClasss: "activate"
@@ -92,4 +93,79 @@ Sortable.create(tareasVerduras, {
     },
     animation: 150,
     chosenClasss: "activate"
+});
+*/
+////fetch
+////fetch
+////fetch
+//Initial References
+let result = document.getElementById("result");
+let searchBtn = document.getElementById("search-btn");
+let url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
+
+searchBtn.addEventListener("click", () => {
+    let userInp = document.getElementById("user-inp").value;
+    if (userInp.length == 0) {
+        result.innerHTML = `<h3>Input Field Cannot Be Empty</h3>`;
+    } else {
+        fetch(url + "userInp")
+            .then((response) => response.json())
+            .then((data) => {
+                let myMeal = data.meals[0];
+                console.log(myMeal);
+                console.log(myMeal.strMealThumb);
+                console.log(myMeal.strMeal);
+                console.log(myMeal.strArea);
+                console.log(myMeal.strInstructions);
+                let count = 1;
+                let ingredients = [];
+                for (let i in myMeal) {
+                    let ingredient = "";
+                    let measure = "";
+                    if (i.startsWith("strIngredient") && myMeal[i]) {
+                        ingredient = myMeal[i];
+                        measure = myMeal[`strMeasure` + count];
+                        count += 1;
+                        ingredients.push(`${measure} ${ingredient}`);
+                    }
+                }
+                console.log(ingredients);
+
+                result.innerHTML = `
+    <img src=${myMeal.strMealThumb}>
+    <div class="details">
+        <h2>${myMeal.strMeal}</h2>
+        <h4>${myMeal.strArea}</h4>
+    </div>
+    <div id="ingredient-con"></div>
+    <div id="recipe">
+        <button id="hide-recipe">X</button>
+        <pre id="instructions">${myMeal.strInstructions}</pre>
+    </div>
+    <button id="show-recipe">View Recipe</button>
+    `;
+                let ingredientCon = document.getElementById("ingredient-con");
+                let parent = document.createElement("ul");
+                let recipe = document.getElementById("recipe");
+                let hideRecipe = document.getElementById("hide-recipe");
+                let showRecipe = document.getElementById("show-recipe");
+
+                ingredients.forEach((i) => {
+                    let child = document.createElement("li");
+                    child.innerText = i;
+                    parent.appendChild(child);
+                    ingredientCon.appendChild(parent);
+                });
+
+                hideRecipe.addEventListener("click", () => {
+                    recipe.style.display = "none";
+                });
+                showRecipe.addEventListener("click", () => {
+                    recipe.style.display = "block";
+                });
+            })
+            .catch(() => {
+                result.innerHTML = `<h3>Invalid Input</h3>`;
+            });
+    }
 });
